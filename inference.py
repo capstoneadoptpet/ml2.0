12bf403bb63ac68439ec6db3f93a2ce1ba165251
deffinpurnama <deffinpurnamanoer@gmail.com>
2025-06-12 19:36:34 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,16 +2,21 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing import image
 
-model = tf.keras.models.load_model(r'C:\AdoptHouse_ImageModel\model_klasifikasi_hewan.h5')
-img_path = r'C:\AdoptHouse_ImageModel\dog1.jpg'
+# Load model
+model = tf.keras.models.load_model('model_klasifikasi_hewan.h5')
 
-img = image.load_img(img_path, target_size=(128, 128))  # Sesuaikan dengan input model
+# Ganti dengan nama file gambar Anda sendiri
+img_path = 'your_image.jpg'  # Contoh: 'kelinci1.jpg'
+
+# Load dan proses gambar
+img = image.load_img(img_path, target_size=(128, 128))
 img_array = image.img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0)
 img_array = img_array / 255.0
 
+# Prediksi
 predictions = model.predict(img_array)
-class_names = ['Anjing', 'Kelinci', 'Kucing']  # Ganti sesuai urutan kelasmu
+class_names = ['Anjing', 'Kelinci', 'Kucing']
 predicted_class = class_names[np.argmax(predictions)]
 confidence = round(100 * np.max(predictions), 2)
 
